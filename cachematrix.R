80554cf813dbdca104964c89ef6eeb365280b991
mloxton <mloxton@hotmail.com>
2015-03-03 09:12:21 -0500

    New code
    My attempt to satisfy  problem I don't understand

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions together receive a matrix and perform a matrix inversion
+## If the matrix cannot be inverted, the function stops with a warning.
+## The <<- operator is used in case the inversion has already been done
 
-## Write a short comment describing this function
+## This function builds the recipient matrix structure, and tests to see
+## if it has been done before
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  my.matrix<<-x #have we seen you before
+  cacheSolve(my.matrix)
 }
 
 
-## Write a short comment describing this function
+## This function tests if the matrix can be inverted, and then 
+## uses solve() to perfrom an inversion
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  test<-det(x) #set up a test to see if we can invert this matrix
+  if (round(test,digit=5)){ #very small values are effectively zero
+    stop("Determinant is zero, matrix cannot be inverted")
+  }
+  inv.x<<-solve(x) #invert the matrix using solve, but <<- in case we did it before
+  inv.x #make it the last value so that the function returns it
 }
